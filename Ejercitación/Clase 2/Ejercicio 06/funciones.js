8e1aa6b77d2bd8dfedc659c1b43dff51d16c52ac
alumno <alumno>
2019-03-22 12:08:46 -0300

    Clase 02

---
+++ Ejercitación/Clase 2/Ejercicio 06/funciones.js
@@ -0,0 +1,6 @@
+"use strict";
+var miFuncion = function obtenerCubo(num) { return Math.pow(num, 3); };
+console.log(miFuncion(2));
+miFuncion = function (i) { Math.pow(i, 3); };
+console.log(miFuncion(10));
+//# sourceMappingURL=funciones.js.map
---
+++ Ejercitación/Clase 2/Ejercicio 06/funciones.ts
@@ -0,0 +1,5 @@
+
+var miFuncion : Function = function obtenerCubo(num : number ) : number {    return num ** 3; }
+console.log( miFuncion(2));
+miFuncion = (i : number ) =>{ i**3 };
+console.log(miFuncion(10));
